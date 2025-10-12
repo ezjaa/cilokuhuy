c7c34b72c5b9fb29c3a1f3604b0d9196ba399178
ezjaa <reza200606@gmail.com>
2025-10-12 17:07:18 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+
+  function handleGrabClick(event) {
+    
+    fbq('track', 'ClickGrabFood');
+    
+    event.preventDefault();
+
+    alert("Maaf yaaa, saat ini kami belum mendaftar di GrabFood.\nKami akan segera mendaftar. Makasih yaa! 😊");
+  }
+
+
