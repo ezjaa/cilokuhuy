f8da05e112794383bb59796a7bcc52457db0dc77
ezjaa <reza200606@gmail.com>
2025-10-19 02:45:26 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+const maaf = document.querySelector('.grab-food');
+
+maaf.addEventListener('click', (event) => {
+    event.preventDefault();
+    alert('Maaf yaaa! Saat ini kami belum terdaftar di GrabFood. Kami sedang dalam proses pendaftaran. Terima Kasih!')
+});
